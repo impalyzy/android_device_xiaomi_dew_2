bed58d8c625e82b43492a064fe1682c00afe78cf
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 20:24:52 +0800

    Delete device.mk

--- device.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := device/xiaomi/dew
-
-# Inherit base recovery properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-
-PRODUCT_NAME := twrp_dew
-PRODUCT_DEVICE := dew
