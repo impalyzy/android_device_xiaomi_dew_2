2fed5fd3634d02fffa3dd3a4e7e8f8e875d5cb58
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 21:54:02 +0800

    my bad i feel tired now

--- fox_dew.mk
+++ fox_dew.mk
@@ -1,11 +1,13 @@
 LOCAL_PATH := device/xiaomi/dew
 
-# Inherit base recovery properties
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Inherit AOSP base product configuration for Android 14
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
-# Inherit OrangeFox configuration
+# Inherit OrangeFox common configuration
 $(call inherit-product, vendor/recovery/config/common.mk)
 
+# Device Identifier Flags
 PRODUCT_NAME := fox_dew
 PRODUCT_DEVICE := dew
 PRODUCT_BRAND := Xiaomi
@@ -15,13 +17,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 # Maintainer Info
 OF_MAINTAINER := impalyzy
 
-# Display & Resolution (720x1600)
+# Display & UI Dimensions (720x1600)
 OF_SCREEN_H := 1600
 OF_STATUS_H := 80
 OF_STATUS_INDENT_LEFT := 48
 OF_STATUS_INDENT_RIGHT := 48
-
-# Backlight / Brightness configuration
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 4095
-TW_DEFAULT_BRIGHTNESS := 2048
