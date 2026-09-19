573b282e17016039c2f002f827c27aa6302f717c
yzy <97025017+impalyzy@users.noreply.github.com>
2026-09-19 19:57:46 +0800

    Add files via upload

---
+++ device/xiaomi/dew/device.mk
@@ -0,0 +1,7 @@
+LOCAL_PATH := device/xiaomi/dew
+
+# Inherit base recovery properties
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+PRODUCT_NAME := twrp_dew
+PRODUCT_DEVICE := dew
